741c9a4d3b888e812df4905e616c5a5b39ee21da
Manuel Vila <mvila@3base.com>
2015-01-03 11:36:18 +0800

    add local config feature

--- index.js
+++ index.js
@@ -2,61 +2,72 @@
 
 var os = require('os');
 var _ = require('lodash');
-var config = require('kinda-config').create();
-
+var KindaObject = require('kinda-object');
+var mainConfig = require('kinda-config').create();
 var moduleConfig = require('kinda-config').get('kinda-notification');
 
-var sender = moduleConfig.sender || config.name + '@' + os.hostname();
-
-var kindaNotification = {};
-
-kindaNotification.registerService = function(name, service) {
-  if (!this._services) this._services = {};
-  this._services[name] = service;
-};
-
-kindaNotification.registerService(
-  'slack-incoming-webhook',
-  require('./services/slack-incoming-webhook')
-);
-
-kindaNotification.getService = function(name) {
-  var service = this._services[name];
-  if (!service) throw new Error("unknown service '" + name + "'");
-  return service;
-};
-
-kindaNotification.getServiceInstance = function(name, options) {
-  if (!this._serviceInstanceRecords) this._serviceInstanceRecords = [];
-  var instanceRecord = _.find(this._serviceInstanceRecords, function(service) {
-    return service.name === name && _.isEqual(service.options, options);
+var KindaNotification = KindaObject.extend('KindaNotification', function() {
+  this.setCreator(function(localConfig) {
+    var config = moduleConfig;
+    if (localConfig) {
+      config = _.cloneDeep(config);
+      _.merge(config, localConfig, function(a, b) { // don't merge arrays
+        return _.isArray(a) || _.isArray(b) ? b : undefined;
+      });
+    }
+    this.config = config;
+    this.sender = config.sender || mainConfig.name + '@' + os.hostname();
   });
-  if (instanceRecord) return instanceRecord.instance;
-  var service = this.getService(name);
-  var instance = service.create(sender, options);
-  instanceRecord = {
-    name: name,
-    options: options,
-    instance: instance
+
+  this._services = {}; // Shared across all instances
+
+  this.registerService = function(name, service) {
+    this._services[name] = service;
   };
-  this._serviceInstanceRecords.push(instanceRecord);
-  return instance;
-};
 
-kindaNotification.send = function(message) {
-  if (!moduleConfig) return;
-  var targets = moduleConfig.targets;
-  if (!targets) return;
-  targets.forEach(function(target) {
-    var instance = this.getServiceInstance(target.service, target.options);
-    instance.send(message);
-  }.bind(this));
-};
+  this.getService = function(name) {
+    var service = this._services[name];
+    if (!service) throw new Error("unknown service '" + name + "'");
+    return service;
+  };
 
-var KindaNotification = {
-  create: function() {
-    return kindaNotification;
-  }
-};
+  this.registerService(
+    'slack-incoming-webhook',
+    require('./services/slack-incoming-webhook')
+  );
+
+  this._serviceInstanceRecords = []; // Shared across all instances
+
+  this.getServiceInstance = function(name, sender, options) {
+    var instanceRecord = _.find(this._serviceInstanceRecords, function(record) {
+      return (
+        record.name === name &&
+        record.sender === sender &&
+        _.isEqual(record.options, options)
+      );
+    });
+    if (instanceRecord) return instanceRecord.instance;
+    var service = this.getService(name);
+    var instance = service.create(sender, options);
+    instanceRecord = {
+      name: name,
+      sender: sender,
+      options: options,
+      instance: instance
+    };
+    this._serviceInstanceRecords.push(instanceRecord);
+    return instance;
+  };
+
+  this.send = function(message) {
+    if (!this.config.targets) return;
+    this.config.targets.forEach(function(target) {
+      var instance = this.getServiceInstance(
+        target.service, this.sender, target.options
+      );
+      instance.send(message);
+    }.bind(this));
+  };
+});
 
 module.exports = KindaNotification;
